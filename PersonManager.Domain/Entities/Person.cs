2dcb18beabf72bc9e152d17d350681292089da2b
Daniel Silva <81590291+dasidi2002@users.noreply.github.com>
2025-03-17 18:44:54 -0300

    fix : adição DateTime

--- PersonManager.Domain/Entities/Person.cs
+++ PersonManager.Domain/Entities/Person.cs
@@ -1,4 +1,5 @@
-﻿namespace PersonManager.Domain.Entities
+﻿// PersonManager.Domain/Entities/Person.cs
+namespace PersonManager.Domain.Entities
 {
     public abstract class Person
     {
@@ -6,17 +7,21 @@
         public string Name { get; protected set; }
         public string DocumentNumber { get; protected set; }
         public Address Address { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+        public DateTime? UpdatedAt { get; protected set; }
 
         protected Person(string name, string documentNumber)
         {
             Id = Guid.NewGuid();
             Name = name;
             DocumentNumber = documentNumber;
+            CreatedAt = DateTime.UtcNow;
         }
 
         public void UpdateAddress(Address address)
         {
             Address = address;
+            UpdatedAt = DateTime.UtcNow;
         }
     }
 }
