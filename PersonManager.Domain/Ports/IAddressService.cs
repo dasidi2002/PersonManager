1b2eba0ec57728a42201594252de4045762b41f8
Daniel Silva <81590291+dasidi2002@users.noreply.github.com>
2025-03-17 18:48:32 -0300

    feat : adição contratos

---
+++ PersonManager.Domain/Ports/IAddressService.cs
@@ -0,0 +1,9 @@
+﻿using PersonManager.Domain.Entities;
+
+namespace PersonManager.Domain.Ports
+{
+    public interface IAddressService
+    {
+        Task<Address> GetAddressByZipCodeAsync(string zipCode);
+    }
+}
---
+++ PersonManager.Domain/Ports/IPersonRepository.cs
@@ -0,0 +1,13 @@
+﻿using PersonManager.Domain.Entities;
+
+namespace PersonManager.Domain.Ports
+{
+    public interface IPersonRepository
+    {
+        Task<Person> GetByIdAsync(Guid id);
+        Task<IEnumerable<Person>> GetAllAsync();
+        Task AddAsync(Person person);
+        Task UpdateAsync(Person person);
+        Task DeleteAsync(Guid id);
+    }
+}
