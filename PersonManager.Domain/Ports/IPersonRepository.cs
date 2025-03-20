2ba442a32b22dced0b0e7166ea3116eece0e772c
Daniel Silva <81590291+dasidi2002@users.noreply.github.com>
2025-03-20 00:51:20 -0300

    fx: ports domain

--- PersonManager.Domain/Ports/IPersonRepository.cs
+++ PersonManager.Domain/Ports/IPersonRepository.cs
@@ -4,10 +4,10 @@ namespace PersonManager.Domain.Ports
 {
     public interface IPersonRepository
     {
-        Task<Person> GetByIdAsync(Guid id);
+        Task<Person> GetByIdAsync(int id);
         Task<IEnumerable<Person>> GetAllAsync();
-        Task AddAsync(Person person);
+        Task<Person> AddAsync(Person person);
         Task UpdateAsync(Person person);
-        Task DeleteAsync(Guid id);
+        Task DeleteAsync(int id);
     }
 }
