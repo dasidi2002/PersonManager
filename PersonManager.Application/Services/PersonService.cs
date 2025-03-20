23fd73e4737a66b01741f254216051d0dc28c075
Daniel Silva <81590291+dasidi2002@users.noreply.github.com>
2025-03-20 00:53:20 -0300

    fix: implemetação service

--- PersonManager.Application/Services/PersonService.cs
+++ PersonManager.Application/Services/PersonService.cs
@@ -1,5 +1,6 @@
-﻿using PersonManager.Application.DTOs;
-using PersonManager.Application.Enums;
+﻿using AutoMapper;
+using PersonManager.Application.DTOs;
+using PersonManager.Application.Exceptions;
 using PersonManager.Application.Interfaces;
 using PersonManager.Domain.Entities;
 using PersonManager.Domain.Ports;
@@ -10,111 +11,137 @@ namespace PersonManager.Application.Services
     {
         private readonly IPersonRepository _personRepository;
         private readonly IAddressService _addressService;
+        private readonly IMapper _mapper;
 
-        public PersonService(IPersonRepository personRepository, IAddressService addressService)
+        public PersonService(IPersonRepository personRepository, IAddressService addressService, IMapper mapper)
         {
             _personRepository = personRepository;
             _addressService = addressService;
+            _mapper = mapper;
         }
 
-        public async Task<PersonDto> GetByIdAsync(Guid id)
+        public async Task<PersonDto> GetByIdAsync(int id)
         {
             var person = await _personRepository.GetByIdAsync(id);
-            return MapToDto(person);
+            if (person == null)
+                throw new PersonNotFoundException(id);
+
+            return _mapper.Map<PersonDto>(person);
         }
 
         public async Task<IEnumerable<PersonDto>> GetAllAsync()
         {
             var persons = await _personRepository.GetAllAsync();
-            return persons.Select(MapToDto);
+            return _mapper.Map<IEnumerable<PersonDto>>(persons);
         }
 
-        public async Task<Guid> CreateNaturalPersonAsync(PersonDto personDto)
+        public async Task<PersonDto> CreateNaturalPersonAsync(CreateNaturalPersonDto personDto)
         {
-            var address = await GetAddressAsync(personDto.Address.ZipCode);
+            try
+            {
+                var address = await GetAddressAsync(personDto.ZipCode);
+                var person = _mapper.Map<NaturalPerson>(personDto);
+                person.UpdateAddress(address);
+                var createdPerson = await _personRepository.AddAsync(person);
 
-            var person = new NaturalPerson(
-                personDto.Name,
-                personDto.DocumentNumber,
-                personDto.BirthDate.Value
-            );
-
-            person.UpdateAddress(address);
-            await _personRepository.AddAsync(person);
-
-            return person.Id;
+                return _mapper.Map<PersonDto>(createdPerson);
+            }
+            catch (HttpRequestException)
+            {
+                throw new ZipCodeNotFoundException(personDto.ZipCode);
+            }
         }
 
-        public async Task<Guid> CreateLegalPersonAsync(PersonDto personDto)
+        public async Task<PersonDto> CreateLegalPersonAsync(CreateLegalPersonDto personDto)
         {
-            var address = await GetAddressAsync(personDto.Address.ZipCode);
+            try
+            {
+                var address = await GetAddressAsync(personDto.ZipCode);
+                var person = _mapper.Map<LegalPerson>(personDto);
+                person.UpdateAddress(address);
+                var createdPerson = await _personRepository.AddAsync(person);
 
-            var person = new LegalPerson(
-                personDto.Name,
-                personDto.CompanyName,
-                personDto.DocumentNumber
-            );
-
-            person.UpdateAddress(address);
-            await _personRepository.AddAsync(person);
-
-            return person.Id;
+                return _mapper.Map<PersonDto>(createdPerson);
+            }
+            catch (HttpRequestException)
+            {
+                throw new ZipCodeNotFoundException(personDto.ZipCode);
+            }
         }
 
-        public async Task UpdatePersonAsync(Guid id, PersonDto personDto)
+        public async Task<OperationResponseDto> UpdateNaturalPersonAsync(int id, UpdateNaturalPersonDto personDto)
         {
             var person = await _personRepository.GetByIdAsync(id);
-
             if (person == null)
-                throw new KeyNotFoundException($"Person with id {id} not found");
+                throw new PersonNotFoundException(id);
 
-            var address = await GetAddressAsync(personDto.Address.ZipCode);
-            person.UpdateAddress(address);
+            if (!(person is NaturalPerson naturalPerson))
+                throw PersonTypeException.NotNaturalPerson;
 
-            await _personRepository.UpdateAsync(person);
+            if (!string.IsNullOrEmpty(personDto.ZipCode))
+            {
+                try
+                {
+                    var address = await GetAddressAsync(personDto.ZipCode);
+                    naturalPerson.UpdateAddress(address);
+                }
+                catch (HttpRequestException)
+                {
+                    throw new ZipCodeNotFoundException(personDto.ZipCode);
+                }
+            }
+
+            _mapper.Map(personDto, naturalPerson); // Atualiza apenas os campos fornecidos
+            await _personRepository.UpdateAsync(naturalPerson);
+
+            return _mapper.Map<OperationResponseDto>(naturalPerson);
         }
 
-        public async Task DeletePersonAsync(Guid id)
+        public async Task<OperationResponseDto> UpdateLegalPersonAsync(int id, UpdateLegalPersonDto personDto)
         {
+            var person = await _personRepository.GetByIdAsync(id);
+            if (person == null)
+                throw new PersonNotFoundException(id);
+
+            if (!(person is LegalPerson legalPerson))
+                throw PersonTypeException.NotLegalPerson;
+
+            if (!string.IsNullOrEmpty(personDto.ZipCode))
+            {
+                try
+                {
+                    var address = await GetAddressAsync(personDto.ZipCode);
+                    legalPerson.UpdateAddress(address);
+                }
+                catch (HttpRequestException)
+                {
+                    throw new ZipCodeNotFoundException(personDto.ZipCode);
+                }
+            }
+
+            _mapper.Map(personDto, legalPerson); // Atualiza apenas os campos fornecidos
+            await _personRepository.UpdateAsync(legalPerson);
+
+            return _mapper.Map<OperationResponseDto>(legalPerson);
+        }
+
+        public async Task<OperationResponseDto> DeletePersonAsync(int id)
+        {
+            var person = await _personRepository.GetByIdAsync(id);
+            if (person == null)
+                throw new PersonNotFoundException(id);
+
+            var response = _mapper.Map<OperationResponseDto>(person);
+            response.Message = "Pessoa excluída com sucesso";
+
             await _personRepository.DeleteAsync(id);
+
+            return response;
         }
 
         private async Task<Address> GetAddressAsync(string zipCode)
         {
             return await _addressService.GetAddressByZipCodeAsync(zipCode);
-        }
-
-        private PersonDto MapToDto(Person person)
-        {
-            var dto = new PersonDto
-            {
-                Id = person.Id,
-                Name = person.Name,
-                DocumentNumber = person.DocumentNumber,
-                Address = new AddressDto
-                {
-                    Street = person.Address.Street,
-                    Number = person.Address.Number,
-                    Complement = person.Address.Complement,
-                    Neighborhood = person.Address.Neighborhood,
-                    City = person.Address.City,
-                    State = person.Address.State,
-                    ZipCode = person.Address.ZipCode
-                }
-            };
-
-            if (person is NaturalPerson naturalPerson)
-            {
-                dto.PersonType = PersonType.Natural;
-                dto.BirthDate = naturalPerson.BirthDate;
-            }
-            else if (person is LegalPerson legalPerson)
-            {
-                dto.PersonType = PersonType.Legal;
-                dto.CompanyName = legalPerson.CompanyName;
-            }
-
-            return dto;
         }
     }
 }
